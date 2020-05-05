86cdd3fa89699e782f4410e07bb8b3ebd3d5ce83
ShotaroBaba <bshotaro@gmail.com>
2020-05-05 17:51:49 +1000

    A Vue sample code.

---
+++ src/script.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+
+     testVue = new Vue({
+        el: "#test",
+        data: {
+            testData: {
+                indice: [1,2,3,4,5,6,7,8,9,10]
+            }
+        }
+    });
+
+}
